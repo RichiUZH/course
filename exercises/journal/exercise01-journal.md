2f507829c7f4105329d081fac42ca8e6320d93dd
Robin Stohler <robin.stohler@bitvoodoo.ch>
2017-09-28 18:14:50 +0200

    added a function to evaluate the maximum

--- exercises/journal/exercise01-journal.md
+++ exercises/journal/exercise01-journal.md
@@ -1,13 +1,19 @@
 # Lab Journal: Exercise 1
 
-Authors: Stohler Robin, name surname
+Authors: Stohler Robin, Merian Sybilla, Mühlemann Mark 
 
 This is your lab journal. Use it to protocol what you did (even if it did not work) and to describe how you tried to solve the exercise. In case an experiment did not turn out as expected, describe what you did, what you expected to happen, and what actually happened.
 
 This file is in markdown format and rendered nicely in the web. See [this link](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for further information about how markdown works.
 
 
-As a first approach the given code was used to find a convergence point. As Far as I tested the maximum utility is somewhere around 0.5313999999999999 work time either working more or working less results in a decreased utility.
 
-After maximizing the Utility function a global maximum was found at 0.53125 as we can see with the try and error approach we got very close to the actual maximum.
+
+Following the three approaches chosen for the first exercise:
+
+1. As a first approach the given code was used to find a maximum manually. As Far as we tested the maximum utility is somewhere around 0.5313999999999999 work time either working more or working less results in a decreased utility.
+
+2. After maximizing the Utility function a global maximum was found at 0.53125 as we can see, with the manual try and error approach we got very close to the actual maximum.
+
+3. After that we set up a function that would return the leisure fraction with the maximum utility.
 
--- exercises/src/com/agentecon/exercise1/Hermit.java
+++ exercises/src/com/agentecon/exercise1/Hermit.java
@@ -10,6 +10,8 @@ package com.agentecon.exercise1;
 
 import java.io.IOException;
 import java.net.SocketTimeoutException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import com.agentecon.agent.Endowment;
@@ -28,12 +30,15 @@ import com.agentecon.research.IFounder;
 import com.agentecon.research.IInnovation;
 
 /**
- * An autarkic consumer that produces its own food and does not interact with others.
+ * An autarkic consumer that produces its own food and does not interact with
+ * others.
  */
 public class Hermit extends Consumer implements IFounder {
-
 	private IProductionFunction prodFun;
-	private double workFraction = 0.53125;
+	// private double workFraction = 0.53125;
+	boolean first = true;
+	double maxLeisureFraction=0;
+
 
 	public Hermit(IAgentIdGenerator id, Endowment end, IUtility utility) {
 		super(id, end, utility);
@@ -42,7 +47,8 @@ public class Hermit extends Consumer implements IFounder {
 	@Override
 	public IFirm considerCreatingFirm(IStatistics statistics, IInnovation research, IAgentIdGenerator id) {
 		if (this.prodFun == null) {
-			// instead of creating a firm, the hermit will create a production function for himself
+			// instead of creating a firm, the hermit will create a production function for
+			// himself
 			this.prodFun = research.createProductionFunction(HermitConfiguration.POTATOE);
 		}
 		return null;
@@ -53,19 +59,70 @@ public class Hermit extends Consumer implements IFounder {
 		// Hermit does not trade, produces instead for himself
 		produce(getInventory());
 	}
-	
+/*	public Double getWorkFraction(Inventory inventory) {
+		double utility = 0;
+		double workFraction = 0;
+		double manhour = inventory.getStock(HermitConfiguration.MAN_HOUR).getAmount();
+		HashMap<Double, Double> results = new HashMap<Double, Double>();
+		
+				this.collectDailyEndowment();
+				Inventory testInventory = inventory.hide(HermitConfiguration.MAN_HOUR, workFraction * manhour);
+				prodFun.produce(testInventory);
+				utility = this.consume();
+				results.put(workFraction, utility);
+				workFraction = 0.5;
+				this.collectDailyEndowment();
+		return workFraction;
+	}*/
+			
+			
+	public Double getLeisureFraction(Inventory inventory) {
+		double utility = 0;
+		double leisureFraction = 0;
+		double manhour = inventory.getStock(HermitConfiguration.MAN_HOUR).getAmount();
+
+		HashMap<Double, Double> results = new HashMap<Double, Double>();
+		if (first) {
+			first = false;
+			//test all fractions and add the fraction and the utility into a Hashmap
+			while (leisureFraction < 1) {
+				this.collectDailyEndowment();
+				Inventory testInventory = inventory.hide(HermitConfiguration.MAN_HOUR, leisureFraction * manhour);
+				prodFun.produce(testInventory);
+				utility = this.consume();
+				results.put(leisureFraction, utility);
+				//Step size
+				leisureFraction += 0.00001;
+				System.out.println(leisureFraction+" "+utility);
+			}
+			//get the max entry copied from (https://stackoverflow.com/questions/5911174/finding-key-associated-with-max-value-in-a-java-map)
+			HashMap.Entry<Double, Double> maxEntry = null;
+			for (HashMap.Entry<Double, Double> entry : results.entrySet()) {
+				if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+					maxEntry = entry;
+				}
+			}
+			//reset the day for the next step in the loop
+			this.collectDailyEndowment();
+			maxLeisureFraction=maxEntry.getKey();
+		}
+		
+		return maxLeisureFraction;
+		
+	}
+
 	private void produce(Inventory inventory) {
 		IStock currentManhours = inventory.getStock(HermitConfiguration.MAN_HOUR);
-
 		// Play here. Maybe you find a better fraction than 60%?
 		// getUtilityFunction().getWeights() might help you finding out
 		// how the consumer weighs the utility of potatoes and of leisure
 		// time (man-hours) relative to each other.
 		double plannedLeisureTime;
-		plannedLeisureTime = currentManhours.getAmount()-(currentManhours.getAmount()*workFraction);
-		//workFraction = workFraction + 0.0001;
-		System.out.println("Hello"+workFraction); 
-
+		// Applying the maximized working work ratio
+		this.getUtilityFunction().getLatestExperiencedUtility();
+		
+		//Calculating the maximum utility with different leisure fractions
+		plannedLeisureTime =  currentManhours.getAmount() * getLeisureFraction(inventory);
 
 		// The hide function creates allows to hide parts of the inventory from the
 		// production function, preserving it for later consumption.
@@ -87,16 +144,17 @@ public class Hermit extends Consumer implements IFounder {
 		HermitConfiguration config = new HermitConfiguration(null, 0);
 		Endowment endowment = config.createEndowment();
 		IUtility utilityFunction = config.create(0);
-		
+
 		Hermit bob = new Hermit(new SimpleAgentIdGenerator(), endowment, utilityFunction);
 		int endOfTime = 100; // let world end after 100 days
-		for (int t=0; t<endOfTime; t++) {
+		for (int t = 0; t < endOfTime; t++) {
 			bob.collectDailyEndowment();
 			bob.considerCreatingFirm(null, config, null);
 			bob.tradeGoods(null);
 			List<Quantity> inventoryBeforeConsumption = bob.getInventory().getQuantities();
 			double utility = bob.consume();
-			System.out.println("Bob achieved a utility of " + utility + " on day " + t + ". Inventory before consumption was: " + inventoryBeforeConsumption);
+			System.out.println("Bob achieved a utility of " + utility + " on day " + t
+					+ ". Inventory before consumption was: " + inventoryBeforeConsumption);
 		}
 	}
 
