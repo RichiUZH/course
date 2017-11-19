bcbb7bbd11b60ab3b256b1f91879b35f742d7ea8
Luzius Meisser <luzius.meisser+git@gmail.com>
2017-11-19 23:22:04 +0100

    code hint

--- exercises/src/com/agentecon/exercise9/FarmFactory.java
+++ exercises/src/com/agentecon/exercise9/FarmFactory.java
@@ -2,9 +2,11 @@ package com.agentecon.exercise9;
 
 import com.agentecon.agent.Endowment;
 import com.agentecon.agent.IAgentIdGenerator;
+import com.agentecon.configuration.CapitalConfiguration;
 import com.agentecon.configuration.ILandbuyingFarmFactory;
 import com.agentecon.firm.production.CobbDouglasProductionWithFixedCost;
 import com.agentecon.market.IStatistics;
+import com.agentecon.production.PriceUnknownException;
 
 /**
  * In the beginning of the simulation, your FarmFactory is instantiated. Every 20 days, the method considerCreatingNewFarm is called.
@@ -21,6 +23,14 @@ public class FarmFactory implements ILandbuyingFarmFactory {
 	 * Return a new farm if you want to create one given the market statistics.
 	 */
 	public LandBuyingFarm considerCreatingNewFarm(IAgentIdGenerator id, Endowment end, CobbDouglasProductionWithFixedCost prodFun, IStatistics stats) {
+		// You might want to look at current prices to see whether it makes sense to create a new farm:
+		try {
+			stats.getGoodsMarketStats().getPriceBelief(CapitalConfiguration.MAN_HOUR);
+			stats.getGoodsMarketStats().getPriceBelief(CapitalConfiguration.POTATOE);
+			stats.getGoodsMarketStats().getPriceBelief(CapitalConfiguration.LAND);
+		} catch (PriceUnknownException e) {
+		}
+		
 		if (id.getRand().nextDouble() < 0.1 && farmsCreated < 1000) {
 			this.farmsCreated++;
 			// create a farm with a probability of 1%, so every 2000 days...
