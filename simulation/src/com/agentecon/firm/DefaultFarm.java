38826bcb31c83c884b8dc59a9a8fa435a49be391
Luzius Meisser <luzius.meisser+git@gmail.com>
2017-12-08 00:18:28 +0100

    default farm

--- simulation/src/com/agentecon/firm/DefaultFarm.java
+++ simulation/src/com/agentecon/firm/DefaultFarm.java
@@ -11,7 +11,7 @@ import com.agentecon.market.IStatistics;
 
 public class DefaultFarm extends Farm implements IMarketParticipant {
 	
-	protected static final double DISCOUNT_RATE = 0.1;
+	protected static final double DISCOUNT_RATE = 0.005;
 	
 	private InvestmentStrategy investments;
 
